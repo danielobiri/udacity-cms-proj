49e25b23b819c2a6ea696c6c5323ee528f7bdd7d
Daniel Ananey-Obiri <62302457+danielobiri@users.noreply.github.com>
2025-11-06 14:12:17 -0600

    Update application.py

--- application.py
+++ application.py
@@ -1,14 +1,30 @@
+# """
+# This script runs the FlaskWebProject application using a development server.
+# """
+
+# from os import environ
+# from FlaskWebProject import app
+
+# if __name__ == '__main__':
+#     HOST = environ.get('SERVER_HOST', 'localhost')
+#     try:
+#         PORT = int(environ.get('SERVER_PORT', '5555'))
+#     except ValueError:
+#         PORT = 5555
+#     app.run(HOST, PORT, ssl_context='adhoc')
 """
 This script runs the FlaskWebProject application using a development server.
 """
 
 from os import environ
+
 from FlaskWebProject import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '8000'))
     except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT, ssl_context='adhoc')
+        PORT = 8000
+    # Remove SSL context for Azure App Service
+    app.run(HOST, PORT)
