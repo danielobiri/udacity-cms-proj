e9b6b69e773c7f2d3a971134edf3fc3a3fb6e046
Daniel Ananey-Obiri <daniel.ananeyobiri@chevron.com>
2025-11-06 13:32:55 -0600

    Initial commit - Flask CMS application

---
+++ FlaskWebProject/__init__.py
@@ -0,0 +1,41 @@
+"""
+The flask application package.
+"""
+import logging
+
+from flask import Flask
+from flask_login import LoginManager
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+print('here is config:', Config.BLOB_ACCOUNT)
+app = Flask(__name__)
+app.config.from_object(Config)
+# TODO: Add any logging levels and handlers with app.logger
+
+# Configure logging
+if not app.debug:
+    # Set up logging for production
+    import logging
+    import os
+    from logging.handlers import RotatingFileHandler
+    
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    
+    file_handler = RotatingFileHandler('logs/flaskwebproject.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Flask Web Project startup')
+
+Session(app)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+# import FlaskWebProject.views
